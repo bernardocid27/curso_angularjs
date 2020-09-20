30c749a6f7a2e0e170a8c907e0c7882a5ac4db91
Jose Cid <bernardocid27@gmail.com>
2020-09-20 14:01:58 -0400

    clase 10

---
+++ hola_controller/tarea10/controller.js
@@ -0,0 +1,10 @@
+angular.module("mainModule",[])
+    .filter("removeHtml",function(){
+        return function(texto){
+            return String(texto).replace(/<[^>]+>/gm, '');
+        }
+    })
+    .controller("FiltersController",function($scope){
+        $scope.mi_html = "<p>Hola mundo</p>"
+        $scope.costo = 2;
+    });
